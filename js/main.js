5b625df4098d6cbdc1b5365fea345c11b1b42aad
AcoranGonzalezMoray <acoranlol14@gmail.com>
2023-03-02 09:39:28 +0000

    versiones 14. clean code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-fetch('/PWM-TEMPLATES/component/product.html')
-  .then(response => response.text())
-  .then(data => {
-    const htmlContent = data;
-    const miContenedor = document.getElementById("contenedor-productos");
-    for (let i = 0; i < 16; i++) {
-      miContenedor.innerHTML += htmlContent;
-    }
-    console.log(htmlContent);
-  });
+function cargaProductos () {
+  fetch('/PWM-TEMPLATES/component/product.html')
+    .then(response => response.text())
+    .then(data => {
+      const htmlContent = data;
+      const miContenedor = document.getElementById("contenedor-productos");
+      for (let i = 0; i < 16; i++) {
+        miContenedor.innerHTML += htmlContent;
+      }
+    });
+}
 
+cargaProductos();
 
