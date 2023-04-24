ee4a21763cb7cf245155ffc81c55316c241e3222
santiago140103 <safloresreyna@gmail.com>
2023-04-24 12:30:29 +0100

    Commit tactico

--- src/app/app-dashboard-admin/app-dashboard-admin.component.ts
+++ src/app/app-dashboard-admin/app-dashboard-admin.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ShoppingCartService } from '../services/firestore/shoppingCart.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Product } from '../services/firestore/interfaces/product';
+import { Reserva } from '../services/firestore/interfaces/reserva';
+import { Pedido } from '../services/firestore/interfaces/pedido';
+import { User } from '../services/firestore/interfaces/user';
+
 @Component({
   selector: 'app-app-dashboard-admin',
   templateUrl: './app-dashboard-admin.component.html',
@@ -7,4 +15,18 @@ import { RouterModule } from '@angular/router';
 })
 export class AppDashboardAdminComponent {
 
+  private usersCollection: Observable<User[]> | undefined;
+  public reservas: Reserva[] = []
+   constructor(private firestore: AngularFirestore) {}
+
+   ngOnInit() {
+    this.usersCollection = this.firestore.collection<User>('USUARIOS').valueChanges();
+
+    this.firestore.collection<User>('USUARIOS').get().subscribe(snapshot => {
+      snapshot.forEach(doc => {
+        const user = doc.data() as User;
+        this.reservas.push(user.reservations.every);
+      });
+    });
+  }
 }
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { TextReaderService } from '../services/firestore/text-reader.service';
+import { Text } from '../services/firestore/interfaces/text';
+import { UserService } from '../services/firestore/user.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ImageLoaderService } from '../services/firestore/image-loader.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
 
+export class CarouselComponent {
+  imagenes : string[] = []
+
+  constructor (private storage: AngularFireStorage,
+    private imageLoader: ImageLoaderService,) {
+
+  }
+
+  ngOnInit() {
+    this.imageLoader.getRef('carouselActive.jpg').getDownloadURL().subscribe(url => {
+      this.imagenes.push(url);
+    });
+    this.imageLoader.getRef('chocolatesVentana.jpg').getDownloadURL().subscribe(url => {
+      this.imagenes.push(url);
+    });
+    this.imageLoader.getRef('instalaciones.jpg').getDownloadURL().subscribe(url => {
+      this.imagenes.push(url);
+    });
+    console.log(this.imagenes[1])
+    
+  }
 }
